cbc406f10e2a37ee09ae07f4187930773aff6458
CoderXC <xiaobing_mailbox@163.com>
2016-06-28 13:28:38 +0800

    Initial Commit

---
+++ XCConfigureShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCConfigureShare
+//
+//  Created by 小蔡 on 16/6/28.
+//  Copyright © 2016年 小蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCConfigureShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCConfigureShare
+//
+//  Created by 小蔡 on 16/6/28.
+//  Copyright © 2016年 小蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
